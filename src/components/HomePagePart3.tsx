585fb2dfe177d3731855d21a8cf038614a894d09
adts912 <shakyaadts@gmail.com>
2024-02-15 17:21:42 +0545

    added sliders

--- src/components/HomePagePart3.tsx
+++ src/components/HomePagePart3.tsx
@@ -1,7 +1,50 @@
 import img1 from "../images/WALLPAPER/1.jpg";
 import img2 from "../images/WALLPAPER/1.jpg";
+import img6 from "../images/WALLPAPER/2022061712314289-5973731049117497921-vFnRwhx-0sizeoriginal-1.jpg";
+import img3 from "../images/WALLPAPER/african-safari.jpg";
+import img4 from "../images/WALLPAPER/DSCF8131-2.jpg";
+import img5 from "../images/WALLPAPER/DSCF0029.jpg";
+// import img7 from "../images/WALLPAPER/"
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
+const Images = [
+  {
+    image: img3,
+  },
+  {
+    image: img4,
+  },
+  {
+    image: img5,
+  },
+  {
+    image: img6,
+  },
+  // {
+  //   image: img7,
+  // }
+];
 const HomePagePart3 = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <>
       <div className="bg-white w-screen">
@@ -11,33 +54,13 @@ const HomePagePart3 = () => {
             <div className="border border-gray-600"></div>
           </div>
         </div>
-        <div className="flex mx-20 gap-7">
-          <div className="w-1/2 border-2 border-gray-500 rounded-2xl bg-[#f8edeb] inline">
-            <div className=" p-10  pb-0 flex items-center justify-center">
-              <img src={img1} className="w-full" />
-            </div>
-            <div className="text-center pt-5 px-24 text-[#a09897] text-[24px]">
-              THE ULTIMATE TRAVEL BUCKET LIST FOR 2023
-            </div>
-            <div className="border border-[#a09897] w-auto my-10 mx-10"></div>
-            <div className="text-[#a09897] text-center text-[20px] p-2">
-              LEARN MORE
-            </div>
-          </div>
-          <div className="w-1/2 border-2 border-gray-500 rounded-2xl bg-[#f8edeb] inline">
-            <div className="p-10 pb-0 flex items-center justify-center">
-              <img src={img2} className="w-full" />
-            </div>
-            <div className="text-center pt-5 px-24 text-[#a09897] text-[24px]">
-              THE ULTIMATE TRAVEL BUCKET LIST FOR 2023
-            </div>
-            <div className="border border-[#a09897] w-auto my-10 mx-10"></div>
-            <div className="text-[#a09897] text-center text-[20px] p-2">
-              LEARN MORE
-            </div>
-          </div>
-        </div>
+        <Carousel responsive={responsive} infinite={true} className="mb-10">
+          {Images.map((item, index) => (
+            <img key={index} src={item.image} className="w-full h-[500px]" />
+          ))}
+        </Carousel>
       </div>
+      {/* </div> */}
     </>
   );
 };
